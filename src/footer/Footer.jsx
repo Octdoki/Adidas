34b25e66b90c2a4510837dee75bc541e68a56ee4
Doyeon Kim <aza98az@naver.com>
2024-03-25 18:45:11 +0900

    Update Footer.jsx

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,15 +1,32 @@
-import { FooterWrap } from "./FooterStyle";
+import { FooterBottom, FooterWrap } from "./FooterStyle";
 
  
 const Footer = () => {
     return (
         <FooterWrap>
             <div className="inner">
-                <div>
-                쿠키 설정 | 구매 이용약관 | 개인정보 처리방침 | 아디클럽 이용약관 | 사업자정보확인 | 공정거래 자율준수 | 채무지급보증안내
-                </div>
-                <p>http://localhost:5173/</p>
+              
+                <ul>
+                    <li><a href="#">문의하기</a></li>
+                    <li><a href="brand.html">브랜드 소개</a></li>
+                    <li><a href="#">이용약관</a></li>
+                    <li><a href="#">개인정보정책</a></li>
+                </ul>
+
+                <FooterBottom>
+                    <p>
+                        상호: 아디다스코리아(유) | 대표자: 대표자: 곽근엽<br />
+                        서울특별시 서초구 서초대로 74길 4, 삼성생명 서초타워 23층 (06620)<br />
+                        이메일: service@onlineshop.adidas.co.kr
+                    </p>
+                    
+                    <ul>
+                        <li><a href="#"><img src="./images/instagram.png" alt="instagram icon" /></a></li>
+                        <li><a href="#"><img src="./images/twitter.svg" alt="twitter icon" /></a></li>
+                    </ul>
+                </FooterBottom>
             </div>
+            
         </FooterWrap>
     );
 };
